e4eaa25655ef8968534250877648775de274ca9c
Daisy Chen <qoo8857300@gmail.com>
2021-06-22 22:12:41 +0800

    set signin style

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-    <router-link
-      class="navbar-brand"
-      to="/"
-    >
-      餐廳評論網
-    </router-link>
+    <router-link class="navbar-brand" to="/"> 餐廳評論網 </router-link>
 
     <button
       class="navbar-toggler"
@@ -19,33 +14,68 @@
       <span class="navbar-toggler-icon" />
     </button>
 
-    <div
-      id="navbarSupportedContent" 
-      class="navbar-collapse collapse"
-    >
+    <div id="navbarSupportedContent" class="navbar-collapse collapse">
       <div class="ml-auto d-flex align-items-center">
         <!-- is user is admin -->
-        <router-link
-             to="#" 
-             class="text-white mr-3"
-        >
-         管理員後台
-       </router-link>
+        <router-link v-if="currentUser.isAdmin" to="#" class="text-white mr-3">
+          管理員後台
+        </router-link>
 
         <!-- is user is login -->
-          <router-link
-             to="#" 
-             class="text-white mr-3"
-          > 
-           使用者 您好
-         </router-link>
-         <button
-           type="button" 
-           class="btn btn-sm btn-outline-success my-2 my-sm-0"
-          >
-           登出
-          </button>
+        <template v-if="isAuthenticated">
+        <router-link to="#" class="text-white mr-3">
+          {{ currentUser.name || "使用者" }} 您好
+        </router-link>
+        <button
+          type="button"
+          class="btn btn-sm btn-outline-success my-2 my-sm-0"
+        >
+          登出
+        </button>
+        </template>
       </div>
     </div>
   </nav>
 </template>
+<script>
+// ./src/components/Navbar.vue
+// seed data
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: "管理者",
+    email: "root@example.com",
+    image: "https://i.pravatar.cc/300",
+    isAdmin: true,
+  },
+  isAuthenticated: true,
+};
+
+export default {
+  // Vue 會在沒有資料時使用此預設值
+  data() {
+    return {
+      currentUser: {
+        id: -1,
+        name: "",
+        email: "",
+        image: "",
+        isAdmin: false,
+      },
+      isAuthenticated: false,
+    };
+  },
+  created() {
+    this.fetchUser();
+  },
+  methods: {
+    fetchUser() {
+      this.currentUser = {
+        ...this.currentUser,
+        ...dummyUser.currentUser,
+      };
+      this.isAuthenticated = dummyUser.isAuthenticated;
+    },
+  },
+};
+</script>
