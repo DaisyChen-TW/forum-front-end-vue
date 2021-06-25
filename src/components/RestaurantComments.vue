e03a17b1c8f7c186dc9bd4fd680460f4e7cba3a1
Daisy Chen <qoo8857300@gmail.com>
2021-06-26 01:34:29 +0800

    add handleDeleteButtonClick

--- src/components/RestaurantComments.vue
+++ src/components/RestaurantComments.vue
@@ -4,7 +4,7 @@
 
     <div v-for="comment in restaurantComments" :key="comment.id">
       <blockquote class="blockquote mb-0">
-        <button v-if="currentUser.isAdmin" type="button" class="btn btn-danger float-right">Delete</button>
+        <button v-if="currentUser.isAdmin" type="button" class="btn btn-danger float-right" @click.stop.prevent="handleDeleteButtonClick(comment.id)">Delete</button>
         <h3>
           <a href="#">
             {{ comment.User.name }}
@@ -43,6 +43,14 @@ export default {
       currentUser: dummyUser.currentUser,
     };
   },
-  mixins: [fromNowFilter]
+  mixins: [fromNowFilter],
+  methods: {
+    handleDeleteButtonClick (commentId) {
+      console.log('handleDeleteButtonClick', commentId)
+      // TODO: 請求 API 伺服器刪除 id 為 commentId 的評論
+      // 觸發父層事件 - $emit( '事件名稱' , 傳遞的資料 )
+      this.$emit('after-delete-comment', commentId)
+    }
+  }
 };
 </script>
