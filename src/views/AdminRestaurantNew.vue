52ae8c87a5cddee0349c8e49062aaf89905c68f3
Daisy Chen <qoo8857300@gmail.com>
2021-06-30 13:03:04 +0800

    set upload image

--- src/views/AdminRestaurantNew.vue
+++ src/views/AdminRestaurantNew.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container py-5">
-    <AdminRestaurantForm />
+    <AdminRestaurantForm @after-submit="handleAfterSubmit" />
     
   </div>
 </template>
@@ -12,6 +12,14 @@ export default {
   name: 'AdminRestaurantNew',
   components: {
     AdminRestaurantForm
+  },
+  methods: {
+    handleAfterSubmit (formData) {
+      // 透過 API 將表單資料送到伺服器
+      for (let [name, value] of formData.entries()) {
+        console.log(name + ': ' + value)
+      }
+    }
   }
 }
 </script>
