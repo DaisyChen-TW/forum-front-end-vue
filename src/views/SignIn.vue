8c177033965429f39cba23e7cf23faac7605ca6d
Daisy Chen <qoo8857300@gmail.com>
2021-07-02 01:45:21 +0800

    API：add restaurant.js

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -64,7 +64,7 @@ export default {
     };
   },
   methods: {
-    async handleSubmit(e) {
+    async handleSubmit() {
       try {
         if (!this.email || !this.password) {
           Toast.fire({
